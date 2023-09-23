8780140643713e926c4c31e3cb5f6c354204aab4
Abul Chowdhury <abulchwdhury@gmail.com>
2023-09-24 00:09:24 +0100

    updated files

--- CalcResourceFactory.cpp
+++ CalcResourceFactory.cpp
@@ -1,62 +1,50 @@
 #include "CalcResourceFactory.h"
-#include "json.hpp"
 
 #include <sstream>
 #include <iomanip>
+#include "json.hpp"
 
 using namespace nlohmann;
-
 
 CalcResourceFactory::CalcResourceFactory() {
     _resource = make_shared<Resource>();
     _resource->set_path(
-        "/{operation: add|subtract|multuply|divide}"
+        "/{operation: add|subtract|multiply|divide}"
         "/{num1: [-+]?[0-9]*\\.?[0-9]*}"
-        "/{num2: [-+]?[0-9]*\\.?[0-9]*}"
-    );
-    _resource->set_method_handler("GET", [&](const shared_ptr<Session> session){
-        get_handler(session);
-    });
-
+        "/{num2: [-+]?[0-9]*\\.?[0-9]*}");
+    _resource->set_method_handler("GET", 
+        [&](const auto session) {
+            get_handler(session);
+        });
 }
 
-// tuple<float, float, string> CalcResourceFactory::get_path_parameters(
-//     const shared_ptr<Session> session) const {
-    
-//         const auto& request = session->get_request();
-//         auto operation = request->get_path_parameters("operation");
-//         auto num1 = atof(request->get_path_parameters("num1").c_str());
-//         auto num2 = atof(request->get_path_parameters("num2").c_str());
-//         return make_tuple(num1, num2, operation);
-// }
-
-
-
-tuple<float, float, string> CalcResourceFactory::get_path_parameters(
-    const shared_ptr<Session> session) const {
-        const auto& request = session->get_request();
-        const auto operation = request->get_path_parameter("operation");
-        auto num1 = atof(request->get_path_parameter("num1").c_str());
-        auto num2 = atof(request->get_path_parameter("num2").c_str());
-        return make_tuple(num1, num2, operation);
-} 
+shared_ptr<Resource> CalcResourceFactory::get_resource() const {
+        return _resource;
+}
 
 float CalcResourceFactory::calculate(float num1, float num2, string operation) {
-    if (operation == "add") {
+    if(operation == "add") {
         return num1 + num2;
     }
-
-    else if (operation == "subtract") {
+    else if(operation == "subtract") {
         return num1 - num2;
     }
-    else if (operation == "multiply") {
+    else if(operation == "multiply") {
         return num1 * num2;
     }
-    else if (operation == "divide") {
+    else if(operation == "divide") {
         return num1 / num2;
     }
-};
+}
 
+tuple<float, float, string> CalcResourceFactory::get_path_parameters(
+        const shared_ptr<Session> session) const {
+    const auto& request = session->get_request();
+    const auto operation = request->get_path_parameter("operation");
+    auto num1 = atof(request->get_path_parameter("num1").c_str());
+    auto num2 = atof(request->get_path_parameter("num2").c_str());
+    return make_tuple(num1, num2, operation);
+} 
 
 string CalcResourceFactory::to_json(float result) {
     ostringstream str_stream;
@@ -64,11 +52,13 @@ string CalcResourceFactory::to_json(float result) {
     json jsonResult = {
         {"result", str_stream.str()}
     };
-    return jsonResult.dump();}
+    return jsonResult.dump();
+}
 
 void CalcResourceFactory::get_handler(const shared_ptr<Session> session) {
     const auto [num1, num2, operation] = get_path_parameters(session);
-    const auto result = calculate(num1, num2, operation);
-
-
+    auto result = calculate(num1, num2, operation);
+    auto content = to_json(result);
+    session->close(OK, content, 
+        {{"Content-Length", to_string(content.size())}});
 }
---
+++ oldFiles/CalcResourceFactory.cpp
@@ -0,0 +1,74 @@
+#include "CalcResourceFactory.h"
+#include "json.hpp"
+
+#include <sstream>
+#include <iomanip>
+
+using namespace nlohmann;
+
+
+CalcResourceFactory::CalcResourceFactory() {
+    _resource = make_shared<Resource>();
+    _resource->set_path(
+        "/{operation: add|subtract|multuply|divide}"
+        "/{num1: [-+]?[0-9]*\\.?[0-9]*}"
+        "/{num2: [-+]?[0-9]*\\.?[0-9]*}"
+    );
+    _resource->set_method_handler("GET", [&](const shared_ptr<Session> session){
+        get_handler(session);
+    });
+
+}
+
+// tuple<float, float, string> CalcResourceFactory::get_path_parameters(
+//     const shared_ptr<Session> session) const {
+    
+//         const auto& request = session->get_request();
+//         auto operation = request->get_path_parameters("operation");
+//         auto num1 = atof(request->get_path_parameters("num1").c_str());
+//         auto num2 = atof(request->get_path_parameters("num2").c_str());
+//         return make_tuple(num1, num2, operation);
+// }
+
+
+
+tuple<float, float, string> CalcResourceFactory::get_path_parameters(
+    const shared_ptr<Session> session) const {
+        const auto& request = session->get_request();
+        const auto operation = request->get_path_parameter("operation");
+        auto num1 = atof(request->get_path_parameter("num1").c_str());
+        auto num2 = atof(request->get_path_parameter("num2").c_str());
+        return make_tuple(num1, num2, operation);
+} 
+
+float CalcResourceFactory::calculate(float num1, float num2, string operation) {
+    if (operation == "add") {
+        return num1 + num2;
+    }
+
+    else if (operation == "subtract") {
+        return num1 - num2;
+    }
+    else if (operation == "multiply") {
+        return num1 * num2;
+    }
+    else if (operation == "divide") {
+        return num1 / num2;
+    }
+};
+
+
+string CalcResourceFactory::to_json(float result) {
+    ostringstream str_stream;
+    str_stream << result;
+    json jsonResult = {
+        {"result", str_stream.str()}
+    };
+    return jsonResult.dump();}
+
+void CalcResourceFactory::get_handler(const shared_ptr<Session> session) {
+    const auto [num1, num2, operation] = get_path_parameters(session);
+    const auto result = calculate(num1, num2, operation);
+
+
+}
