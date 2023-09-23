7e08153d1d65d65e22b48aafd86d4aa084d29a5c
Sabir Chowdhury <sabir@wondersofmaths.com>
2023-09-23 23:59:58 +0100

    cpp files

--- CalcResourceFactory.cpp
+++ CalcResourceFactory.cpp
@@ -1,4 +1,11 @@
 #include "CalcResourceFactory.h"
+#include "json.hpp"
+
+#include <sstream>
+#include <iomanip>
+
+using namespace nlohmann;
+
 
 CalcResourceFactory::CalcResourceFactory() {
     _resource = make_shared<Resource>();
@@ -13,15 +20,26 @@ CalcResourceFactory::CalcResourceFactory() {
 
 }
 
-tuple<float, float, string> CalcResourceFactory::get_path_parameters(
-    const shared_ptr<Session> session) {
+// tuple<float, float, string> CalcResourceFactory::get_path_parameters(
+//     const shared_ptr<Session> session) const {
     
+//         const auto& request = session->get_request();
+//         auto operation = request->get_path_parameters("operation");
+//         auto num1 = atof(request->get_path_parameters("num1").c_str());
+//         auto num2 = atof(request->get_path_parameters("num2").c_str());
+//         return make_tuple(num1, num2, operation);
+// }
+
+
+
+tuple<float, float, string> CalcResourceFactory::get_path_parameters(
+    const shared_ptr<Session> session) const {
         const auto& request = session->get_request();
-        auto operation = request->get_path_parameters("operation");
-        auto num1 = atof(request->get_path_parameters("num1").c_str());
-        auto num2 = atof(request->get_path_parameters("num2").c_str());
+        const auto operation = request->get_path_parameter("operation");
+        auto num1 = atof(request->get_path_parameter("num1").c_str());
+        auto num2 = atof(request->get_path_parameter("num2").c_str());
         return make_tuple(num1, num2, operation);
-}
+} 
 
 float CalcResourceFactory::calculate(float num1, float num2, string operation) {
     if (operation == "add") {
@@ -38,6 +56,13 @@ float CalcResourceFactory::calculate(float num1, float num2, string operation) {
         return num1 / num2;
     }
 };
+
+
+string CalcResourceFactory::to_json(float result) {
+    ostringstream str_stream;
+    str_stream << result;
+    json jsonResult
+}
 
 void CalcResourceFactory::get_handler(const shared_ptr<Session> session) {
     const auto [num1, num2, operation] = get_path_parameters(session);
--- calcResourceFactory.h
+++ calcResourceFactory.h
@@ -11,10 +11,10 @@ public:
     shared_ptr<Resource> get_resource() const final;
 
 private:
-
+    
     string to_json(float result);
     float calculate(float num1, float num2, string operation);
-    tuple<float, float, string> get_path_parameters(const shared_ptr<Session> session);
+    tuple<float, float, string> get_path_parameters(const shared_ptr<Session> session) const;
     void get_handler(const shared_ptr<Session> session);
     shared_ptr<Resource> _resource;
     
